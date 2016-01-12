41c7db1cbc337a7531f10a466c11111c0fe6d036
Harald Lapp <h.lapp@clipdealer.de>
2016-01-12 13:27:19 +0100

    initial commit interface for implementing db result classes

---
+++ libs/Db/Device/IResult.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * This file is part of the 'octris/core' package.
+ *
+ * (c) Harald Lapp <harald@octris.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Octris\Core\Db\Device;
+
+/**
+ * Interface for database query results.
+ *
+ * @copyright   copyright (c) 2016 by Harald Lapp
+ * @author      Harald Lapp <harald@octris.org>
+ */
+interface IResult extends \Iterator, \Countable
+{
+}
