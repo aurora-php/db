24182e244e4a2f8b727c94a7017932b68babfa46
Harald Lapp <h.lapp@clipdealer.de>
2021-08-23 13:54:46 +0200

    strict types, improvements

--- libs/Db/Device.php
+++ libs/Db/Device.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 /*
  * This file is part of the 'octris/db' package.
  *
@@ -11,71 +13,85 @@
 
 namespace Octris\Db;
 
+use \Octris\Db\Device\Type;
+
 /**
  * Database devices base class.
  *
- * @copyright   copyright (c) 2012-2018 by Harald Lapp
+ * @copyright   copyright (c) 2012-present by Harald Lapp
  * @author      Harald Lapp <harald@octris.org>
  */
 abstract class Device
 {
     /**
-     * Storage for host configurations.
+     * Registry for host configurations.
      *
      * @type    array
      */
-    protected $hosts = array(
-        \Octris\Db::DB_MASTER => array(),
-        \Octris\Db::DB_SLAVE  => array()
-    );
+    protected $hosts = [];
 
     /**
      * Active connections.
      *
      * @type    array
      */
-    protected $connections = array();
+    protected $connections = [];
 
     /**
-     * Storage of free database connections.
+     * Registry of free database connections.
      *
      * @type    array
      */
-    protected $pool = array(
-        \Octris\Db::DB_MASTER => array(),
-        \Octris\Db::DB_SLAVE  => array()
-    );
+    protected $pool = [];
 
     /**
      * Constructor.
      */
     public function __construct()
     {
+        $this->hosts[Type::MASTER->value] = [];
+        $this->hosts[Type::SLAVE->value] = [];
+
+        $this->pool[Type::MASTER->value] = [];
+        $this->pool[Type::SLAVE->value] = [];
     }
+
+    /**
+     * Validate connection parameters.
+     *
+     * @param   \Octris\PropertyCollection  $options
+     * @return  bool
+     */
+    abstract protected function validateOptions(\Octris\PropertyCollection $options): bool;
 
     /**
      * Add host configuration of specified type.
      *
-     * @param   string                      $type               Type of host to add (master / slave).
-     * @param   array                       $options            Host configuration options for database master.
+     * @param   string                      $type               Type of host to add.
+     * @param   \Octris\PropertyCollection  $options            Host configuration options for database host.
      * @param   bool                        $master_as_slave    Whether to add a master connection as slave, too.
+     * @throws  \InvalidArgumentException
      */
-    protected function addHost($type, array $options, $master_as_slave = true)
+    public function addHost(Type $type, \Octris\PropertyCollection $options, bool $master_as_slave = true)
     {
-        $this->hosts[$type][] = $options;
+        if (!$this->validateOptions($options)) {
+            throw new \InvalidArgumentException('Invalid options specified');
+        }
 
-        if ($type == \Octris\Db::DB_MASTER && $master_as_slave) {
-            $this->hosts[\Octris\Db::DB_SLAVE][] = $options;
+        $this->hosts[$type->value][] = $options;
+
+        if ($master_as_slave && $type->isMaster()) {
+            $this->hosts[Type::SLAVE->value][] = $options;
         }
     }
 
     /**
      * Create a new database connection for specified configuration options.
      *
-     * @param   array                               $options        Host configuration options.
+     * @param   \Octris\PropertyCollection          $options        Host configuration options.
      * @return  \Octris\Db\Device\ConnectionInterface               Connection to a database.
      */
-    abstract protected function createConnection(array $options);
+    abstract protected function createConnection(\Octris\PropertyCollection $options) : \Octris\Db\Device\ConnectionInterface;
 
     /**
      * Return a database connection of specified type.
@@ -83,28 +99,24 @@ abstract class Device
      * @param   string                              $type           Optional type of connection.
      * @return  \Octris\Db\Device\ConnectionInterface               Connection to a database.
      */
-    public function getConnection($type = \Octris\Db::DB_MASTER)
+    public function getConnection(Type $type = Type::MASTER) : \Octris\Db\Device\ConnectionInterface
     {
-        if ($type != \Octris\Db::DB_MASTER && $type != \Octris\Db::DB_SLAVE) {
-            throw new \Exception('unknown connection type "' . $type . '"');
-        } else {
-            if (!($cn = array_shift($this->pool[$type]))) {
-                // no more connections in the pool, create new one
-                if (count($this->hosts[$type]) == 0) {
-                    throw new \Exception(sprintf('No database configuration available for connection to a "%s" host.', $type));
-                }
-
-                shuffle($this->hosts[$type]);
-
-                $cn = $this->createConnection($this->hosts[$type][0]);
-
-                if (!($cn instanceof \Octris\Db\Device\ConnectionInterface)) {
-                    throw new \Exception('connection handler needs to implement interface "\Octris\Db\Device\ConnectionInterface"');
-                }
+        if (!($cn = array_shift($this->pool[$type->value]))) {
+            // no more connections in the pool, create new one
+            if (count($this->hosts[$type->value]) == 0) {
+                throw new \Exception(sprintf('No database configuration available for connection to a "%s" host.', $type));
             }
 
-            $this->connections[spl_object_hash($cn)] = $type;
+            shuffle($this->hosts[$type->value]);
+
+            $cn = $this->createConnection($this->hosts[$type->value][0]);
+
+            if (!($cn instanceof \Octris\Db\Device\ConnectionInterface)) {
+                throw new \Exception('connection handler needs to implement interface "\Octris\Db\Device\ConnectionInterface"');
+            }
         }
+
+        $this->connections[spl_object_hash($cn)] = $type;
 
         return $cn;
     }
@@ -122,7 +134,11 @@ abstract class Device
             throw new \Exception('Connection is not handled by this device');
         }
 
-        array_push($this->pool[$this->connections[$hash]], $cn);
-        unset($this->connections[$hash]);
+        if (!$cn->doPooling()) {
+            array_push($this->pool[$this->connections[$hash]->value], $cn);
+            unset($this->connections[$hash]);
+        } else {
+            $cn->close();
+        }
     }
 }
